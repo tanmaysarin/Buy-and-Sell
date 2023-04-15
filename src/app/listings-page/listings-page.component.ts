3e2d1e5600e6e52bd75fd3de60c10fe99aac9523
Tanmay Sarin <tanmaysarin@gmail.com>
2023-04-15 00:31:44 -0500

    listings page set

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data'
 
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
   styleUrls: ['./listings-page.component.css']
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit{
+
+  // creating a variable of type Listing and initialize it to empty list
+  listings : Listing[] = [];
+
+  constructor() {};
+
+  ngOnInit(): void {
+    // set the listings list to the fakeListings list data
+    this.listings = fakeListings;
+  }
 
 }
