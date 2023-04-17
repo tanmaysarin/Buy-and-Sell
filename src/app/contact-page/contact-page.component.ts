1ec7a26ef90058525b0389726af1795edf10f7f0
Tanmay Sarin <tanmaysarin@gmail.com>
2023-04-16 23:41:36 -0500

    Creating all basic pages

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit{
+
+  email: string = '';
+  message: string = '';
+  listing = {} as Listing;
+
+  constructor(
+    private route: ActivatedRoute, // Allows listing-details-page to access the url parameter in current path
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id == id)!; // adding an exclamation makes typescript know that the value can never be null
+    this.message = `Hi! I am interested in your ${this.listing.name.toLocaleLowerCase()}!`;
+  }
+
+  sendMessage(): void {
+    alert('Your message has been sent!');
+    this.router.navigateByUrl('/listings');
+  }
 
 }
--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
   templateUrl: './listing-detail-page.component.html',
   styleUrls: ['./listing-detail-page.component.css']
 })
-export class ListingDetailPageComponent {
+export class ListingDetailPageComponent implements OnInit{
+
+  listing = {} as Listing;
+  
+  constructor(
+    private route: ActivatedRoute, // Allows listing-details-page to access the url parameter in current path
+  ) {}
+  
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id)!; // adding an exclamation makes typescript know that the value can never be null
+  }
 
 }
